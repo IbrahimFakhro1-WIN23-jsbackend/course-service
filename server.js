e0c974b548ee6c3a7cea08d7d67b7fbdc000b9df
Ibbefk <ibrahim.fakhro@live.se>
2024-05-24 20:17:15 +0200

    Fix start script and dependencies

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const { createServer } = require('http');
 const { parse } = require('url');
-const http = require('http');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
@@ -11,8 +10,8 @@ app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(process.env.PORT || 3000, err => {
+  }).listen(process.env.PORT || 3000, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:' + (process.env.PORT || 3000));
+    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`);
   });
 });
